2315f3b09431a1c97d9c61f74d73debfc784c9a5
Ícaro Freire <icarovf@outlook.com>
2021-09-20 22:11:55 -0300

    update: primeiras informações

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Homepage acadêmica de Ícaro Vaz Freire no Instituto de Física da Universidade de São Paulo.">
+	<title>Ícaro Freire | IF-USP</title>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/homepage.css">
+</head>
+<body>
+	<div class="page">
+		<div class="column">
+			<div class="container">
+				<div class="left">
+					<div class="menu">
+						<div class="gadget profile">
+							<div class="picture"><img src="images/profile.jpg" alt="icaro vaz freire"></div>
+							<div class="name"><span>Ícaro Vaz Freire</span></div>
+							<div class="description"><p>Bacharelando em Física no IF-USP<br>Bolsista de Iniciação Científica no IAG-USP</p></div>
+						</div>
+					</div>
+				</div>
+				<div class="right">
+					<div class="gadget projects">
+						<h4>Extensão</h4><br>
+						<ul>
+							<li><a href="https://www2.if.usp.br/~hackersp">Hackerspace IFUSP</a></li>
+						</ul>
+					</div>
+					<div class="gadget external-links">
+						<h4>Links externos</h4><br>
+						<ul>
+							<li><a href="https://github.com/ivfreire">GitHub</a></li>
+							<li><a href="https://ivfreire.github.io">GitHub pages</a></li>
+							<li><a href="http://lattes.cnpq.br/2108666866605794">Lattes</a></li>
+						</ul>
+					</div>
+					<div class="gadget contact">
+						<h4>Informações</h4><br>
+						<a href="https://www5.usp.br">Universidade de São Paulo</a><br>
+						<a href="https://portal.if.usp.br/ifusp">Instituto de Física</a><br><br>
+						<p>Rua do Matão, 1371<br>Cidade Universitária - Butantã<br>São Paulo, SP - Brasil</p><br>
+						<ul>
+							<li><a href="mailto:icarovf@usp.br">icarovf@usp.br</a></li>
+							<li><a href="mailto:icarovf@if.usp.br">icarovf@if.usp.br</a></li>
+						</ul>
+					</div>
+					<div class="gadget symbols">
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<footer>
+		<p>São Paulo - 2021</p>
+	</footer>
+</body>
+</html>
