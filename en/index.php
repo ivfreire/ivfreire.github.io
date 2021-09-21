d46240bc5d4e53865e0df2f75a3d0e90223fa8f3
Ícaro Freire <icarovf@outlook.com>
2021-09-21 02:56:19 -0300

    modificado: correções de línguas

--- en/index.php
+++ en/index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="Homepage acadêmica de Ícaro Vaz Freire no Instituto de Física da Universidade de São Paulo.">
+	<meta name="description" content="Icaro Freire's homepage at the University of Sao Paulo in Brazil.">
 	<title>Ícaro Freire | IF-USP</title>
 	<link rel="stylesheet" href="../css/style.css">
 	<link rel="stylesheet" href="../css/homepage.css">
