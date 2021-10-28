9d86ad0b52aee8cc88a346009adb8da073818d40
Ícaro Freire <icarovf@outlook.com>
2021-10-27 22:06:22 -0300

    updates

--- templates/navbar.php
+++ templates/navbar.php
@@ -1,15 +1,16 @@
 <?php
+	$domain = "https://www2.if.usp.br/~icarovf";
 	if (isset($_GET["lang"])) {
-		$domain .= "/en";
 		if ($_GET['lang'] == "en") {
+			$domain .= "/en";
 			echo "
 				<nav class='navigation'>
 					<div class='column'>
 						<ul class='menu'>
-							<a href='/en'><li>Home</li></a>
-							<a href='/../archive'><li>Archive</li></a>
+							<a href='$domain/en'><li>Home</li></a>
+							<a href='$domain/../archive'><li>Archive</li></a>
 						</ul>
-						<a href='/..'>Portuguese</a>
+						<a href='$domain/..'>Portuguese</a>
 					</div>
 				</nav>
 			";
@@ -19,10 +20,10 @@
 			<nav class='navigation'>
 				<div class='column'>
 					<ul class='menu'>
-						<a href='/'><li>Início</li></a>
-						<a href='/archive'><li>Arquivo</li></a>
+						<a href='$domain'><li>Início</li></a>
+						<a href='$domain/archive'><li>Arquivo</li></a>
 					</ul>
-					<a href='en'>English</a>
+					<a href='$domain/en'>English</a>
 				</div>
 			</nav>
 		";
